569c4e1b2a364c0b9a9a9112a5c988abe019dbec
DanielLee <31802525+InfiniteRegen@users.noreply.github.com>
2018-07-24 22:40:45 +0900

    create project

---
+++ FirstProject/app/src/main/java/com/example/a2normous_rollback/firstproject/CoolActivity.java
@@ -0,0 +1,13 @@
+package com.example.a2normous_rollback.firstproject;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CoolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_cool);
+    }
+}
