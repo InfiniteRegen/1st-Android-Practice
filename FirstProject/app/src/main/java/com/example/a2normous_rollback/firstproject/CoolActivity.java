4e5ed1ee38f0744700a9619c35b0e1f031663ac6
DanielLee <31802525+InfiniteRegen@users.noreply.github.com>
2018-08-04 23:46:34 +0900

    Button

--- FirstProject/app/src/main/java/com/example/a2normous_rollback/firstproject/CoolActivity.java
+++ FirstProject/app/src/main/java/com/example/a2normous_rollback/firstproject/CoolActivity.java
@@ -9,5 +9,22 @@ public class CoolActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_cool);
+
+        init();
+        btn_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random dice = new Random();
+                int result = dice.nextInt(6) + 1;
+
+                tv_result.setText(String.valueOf(result));
+            }
+        });
+    }
+
+    public void init() {
+        tv_result = (TextView) findViewById(R.id.tv_result);
+        btn_button = (Button) findViewById(R.id.btn_roll);
     }
 }
